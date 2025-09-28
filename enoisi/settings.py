c97ef6dc1e2fb5239fd00a4db0bb5dc3d567ea16
quemuelbjj <quemuelbadboy@gmail.com>
2025-09-28 18:14:10 -0300

    alowed agora

--- enoisi/settings.py
+++ enoisi/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-@%$w)*u99d##o+@&rsrw^*s%ejjv)5#5g78i0gm9u*pi+q2$4r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quemuel.pythonanywhere.com']
 
 
 # Application definition
