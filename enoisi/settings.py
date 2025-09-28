3240c4709ba49a1304d0e46f4e3aacdc96fb2426
quemuelbjj <quemuelbadboy@gmail.com>
2025-09-28 18:00:12 -0300

    desativei o debug

--- enoisi/settings.py
+++ enoisi/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@%$w)*u99d##o+@&rsrw^*s%ejjv)5#5g78i0gm9u*pi+q2$4r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
