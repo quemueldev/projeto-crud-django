5f5d4e225e9d557045eb52eee83877974d3d7894
quemuelbjj <quemuelbadboy@gmail.com>
2025-09-28 13:16:25 -0300

    Corrigido max_length e outros ajustes

--- crud/models.py
+++ crud/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class cachorro(models.Model):
-    nome = models.CharField()
-    raca = models.CharField()
+    nome = models.CharField(max_length=12)
+    raca = models.CharField(max_length=10)
